9a698174967f19fa8ec35a32ccd6bc08246e3ffc
leiyanhui@gmail.com <joyanhui@qq.com>
2024-01-11 13:41:57 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,416 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:url_launcher/url_launcher.dart'; // 导入 url_launcher 包
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '良友电台-同行频道',
+      theme: ThemeData(
+        // 这里配置亮色主题
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        focusColor: const Color.fromARGB(255, 109, 110, 0), //焦点选中的颜色
+        brightness: Brightness.light,
+        inputDecorationTheme: const InputDecorationTheme(
+          //floatingLabelBehavior: FloatingLabelBehavior.always, // 设置浮动 一直浮动
+          labelStyle: TextStyle(
+            color: Colors.blue, // 设置浮动后的颜色
+          ),
+          border: UnderlineInputBorder(
+            borderRadius: BorderRadius.only(), // 设置外框为直角
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue), // 设置获得焦点时的边框颜色为绿色
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 33, 54, 243),
+          ),
+        ),
+      ),
+      darkTheme: ThemeData(
+        // 这里配置暗黑主题
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        focusColor: const Color.fromARGB(255, 109, 110, 0), //焦点选中的颜色
+        brightness: Brightness.dark,
+        inputDecorationTheme: const InputDecorationTheme(
+          //floatingLabelBehavior: FloatingLabelBehavior.always, // 设置浮动 一直浮动
+          labelStyle: TextStyle(
+            color: Colors.blue, // 设置浮动后的颜色
+          ),
+          border: UnderlineInputBorder(
+            borderRadius: BorderRadius.only(), // 设置外框为直角
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue), // 设置获得焦点时的边框颜色为绿色
+          ),
+        ),
+
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 33, 54, 243),
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system, // 根据系统设置选择主题
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final player = AudioPlayer();
+  var source1 = "https://ly729.out.airtime.pro/ly729_a";
+  var source2 = "https://c20.radioboss.fm/stream/572";
+  var source3 = "https://listen.729ly.net:8001/ly729_a";
+  var source4 = "";
+  final TextEditingController customController = TextEditingController();
+  Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+
+  var isPlaying = false;
+  String? selectedSource;
+
+  _MyHomePageState() {
+    selectedSource = source1; // 默认选中 source1
+  }
+  @override
+  void initState() {
+    super.initState();
+    initPage();
+  }
+
+  Future<void> initPage() async {
+    final prefsInstance = await prefs;
+    source4 = prefsInstance.getString("source4") ?? "";
+  }
+
+  @override
+  void dispose() {
+    player.stop(); // 停止播放
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (isPlaying) {
+                  setState(() {
+                    isPlaying = false;
+                  });
+                  try {
+                    player.stop();
+                  } catch (e) {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoAlertDialog(
+                          title: const Text('提示'),
+                          content: Text('停止错误 ${e.toString()}'),
+                          actions: <Widget>[
+                            CupertinoDialogAction(
+                              child: const Text('确定'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                } else {
+                  setState(() {
+                    isPlaying = true;
+                  });
+                  _play(selectedSource!);
+                }
+              },
+              child: isPlaying
+                  ? const Icon(Icons.pause, size: 200)
+                  : const Icon(Icons.play_arrow, size: 200),
+            ),
+            RadioListTile<String>(
+              title: const Text('Source 1'),
+              value: source1,
+              groupValue: selectedSource,
+              onChanged: (String? value) async {
+                setState(() {
+                  selectedSource = value;
+                });
+                //切换源
+                if (isPlaying) {
+                  _play(selectedSource!);
+                }
+              },
+            ),
+            RadioListTile<String>(
+              title: const Text('Source 2'),
+              value: source2,
+              groupValue: selectedSource,
+              onChanged: (String? value) async {
+                setState(() {
+                  selectedSource = value;
+                });
+                //切换源
+                if (isPlaying) {
+                  _play(selectedSource!);
+                }
+              },
+            ),
+            RadioListTile<String>(
+              title: const Text('Source 3'),
+              value: source3,
+              groupValue: selectedSource,
+              onChanged: (String? value) async {
+                setState(() {
+                  selectedSource = value;
+                });
+                //切换源
+                if (isPlaying) {
+                  _play(selectedSource!);
+                }
+              },
+            ),
+            RadioListTile<String>(
+              title: const Text('自定义'),
+              value: source4,
+              groupValue: selectedSource,
+              onChanged: (String? value) async {
+                if (source4 == "") {
+                  //对话框提示先配置
+                  await showCupertinoDialog(
+                    context: context,
+                    builder: (context) {
+                      return CupertinoAlertDialog(
+                        title: const Text('提示'),
+                        content: const Text('请先配置自定义源'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('确定'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                  return;
+                }
+                setState(() {
+                  selectedSource = value;
+                });
+                //切换源
+                if (isPlaying) {
+                  _play(selectedSource!);
+                }
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 250,
+              child: ElevatedButton(
+                onPressed: () async {
+                  await showCupertinoDialog(
+                    context: context,
+                    builder: (context) {
+                      return CupertinoAlertDialog(
+                        title: const Text('输入自定义源:只支持http/s协议'),
+                        content: CupertinoTextField(
+                          controller: customController,
+                          decoration: const BoxDecoration(
+                            color:
+                                Color.fromARGB(255, 99, 120, 129), // 设置背景颜色为淡蓝色
+                          ),
+                        ),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('确定'),
+                            onPressed: () async {
+                              Navigator.of(context).pop();
+                              //检查 customController.text 是否是一个url地址
+                              Uri? uri;
+                              try {
+                                uri = Uri.parse(customController.text);
+                                // ignore: empty_catches
+                              } catch (e) {}
+
+                              if (uri == null) {
+                                await showCupertinoDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return CupertinoAlertDialog(
+                                      title: const Text('提示'),
+                                      content: const Text('地址格式错误'),
+                                      actions: <Widget>[
+                                        CupertinoDialogAction(
+                                          child: const Text('确定'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                                return;
+                              }
+                              if (customController.text == source1 ||
+                                  customController.text == source2 ||
+                                  customController.text == source3) {
+                                await showCupertinoDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return CupertinoAlertDialog(
+                                      title: const Text('提示'),
+                                      content: const Text('和内置地址一样'),
+                                      actions: <Widget>[
+                                        CupertinoDialogAction(
+                                          child: const Text('确定'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                                return;
+                              }
+                              setState(() {
+                                source4 = customController.text;
+                                selectedSource = customController.text;
+                              });
+                              //写入到缓存
+                              final prefsInstance = await prefs;
+                              prefsInstance.setString("source4", source4);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text("配置自定义源"),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 250,
+              child: Text(
+                  isPlaying ? "▶ 地址:$selectedSource" : "〓 地址:$selectedSource"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              width: 250,
+              child: Text("自定义源 支持http/https协议，支持m3u8源。",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 117, 117, 117),
+                  )),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+                width: 250,
+                child: GestureDetector(
+                  onTap: _launchURL,
+                  child: const Text(
+                    "部分源可能被监管者屏蔽，你可能需要加密上网才能使用",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 117, 117, 117),
+                    ),
+                  ),
+                )),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+                width: 250,
+                child: GestureDetector(
+                  onTap: _launchURL,
+                  child: const Text(
+                    "主页: https://dev.leiyanhui.com/",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 42, 96, 139),
+                    ),
+                  ),
+                )),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+
+  void _launchURL() async {
+    const url = 'https://dev.leiyanhui.com/new/radio-ly/';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url)); // 使用 launchUrl
+    } else {}
+  }
+
+  void _play(String url) async {
+    try {
+      player.stop();
+      player.play(UrlSource(url));
+    } catch (e) {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('提示'),
+            content: Text('播放错误 ${e.toString()}'),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: const Text('确定'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
